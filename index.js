2f422c13a3fc3623c9345ebc95833411b5643c83
MagalDevs <danielgalfla12@gmail.com>
2024-06-04 13:46:54 -0300

    jogo quase pronto

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe')
+const gameover = document.querySelector('.gameover')
+const cloud = document.querySelector('.clouds')
 
 const jump = () => {
     mario.classList.add('jump');
@@ -10,5 +13,30 @@ const jump = () => {
 
     }, 500);
 }
+
+const loop = setInterval(() =>{
+
+    const pipeposition = pipe.offsetLeft;
+    const marioposition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    const cloudposition = +window.getComputedStyle(cloud).right.replace('px', '');
+
+    if (pipeposition <= 120 && pipeposition > 0 && marioposition < 110){
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipeposition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioposition}px`;
+
+        mario.src = 'image/game-over.png';
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
+
+        cloud.style.animation = 'none';
+        cloud.style.right = `${cloudposition}px`
+        gameover.style.visibility = 'visible'
+        clearInterval(loop);
+    }
+
+},10)
 
 document.addEventListener('keydown', jump);
